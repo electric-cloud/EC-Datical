cf54f7c0e122843c12a4ec4839bb01b4df9b125b
Laurent Rochette <lrochette@gmail.com>
2017-06-02 13:17:22 -0500

    Add deploy and forecast report to stage sumary when invoked from a pipeline Add dependency on EC-Admin

--- dsl/procedures/Forecast/steps/copyReportToWorkspace.pl
+++ dsl/procedures/Forecast/steps/copyReportToWorkspace.pl
@@ -5,22 +5,23 @@ use File::Copy;
 # Copyright 2017 Datical Inc.
 #
 #############################################################################
-use strict;
-use English;
-use ElectricCommander;
-use Data::Dumper;
-$| = 1;
 
-my $DEBUG=0;
+$[/plugins/EC-Admin/project/perlHeaderJSON]
 
-# Create a single instance of the Perl access to ElectricCommander
-my $ec = new ElectricCommander({'format' => "json"});
 
-# Check for the OS Type
-my $osIsWindows = $^O =~ /MSWin/;
 copy ('$[/myJob/forecastReport]', ".") || die ("cannot copy the forecast Report: $!");
 
 # to allow for non default workspace
 $ec->setProperty("/myJob/artifactsDirectory", ".");
+
+# Add report to job detail page
 $ec->setProperty("/myJob/report-urls/forecastReport", "/commander/jobSteps/$[jobStepId]/forecastReport.html");
 $ec->setProperty ("summary", "<html>&#160;&#160;&#160;<a href=\"forecastReport.html\">forecast Report</a></html>");
+
+# Add report to Stage sumary if started from  a pipeline
+if (getP('/myPipelineStageRuntime/projectName')) { # Add to Stage summary if running from a pipeline
+    $ec->setProperty("/myPipelineStageRuntime/ec_summary/Datical Forecast Report",
+        "<html><a href=\"/commander/jobSteps/$[jobStepId]/forecastReport.html">Datical Forecast Report</a></html");
+}
+
+$[/plugins/EC-Admin/project/perlLibJSON]
